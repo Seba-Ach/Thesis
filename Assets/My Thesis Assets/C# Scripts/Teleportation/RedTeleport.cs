98f60b1949e7727a5c6436d40a31c5fefe129b5a
Seba <sebas_ach@hotmail.com>
2018-10-25 04:09:51 +0200

    Added more to colloseum

--- Assets/My Thesis Assets/C# Scripts/Teleportation/RedTeleport.cs
+++ Assets/My Thesis Assets/C# Scripts/Teleportation/RedTeleport.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class RedTeleport : MonoBehaviour {
 
     public GameObject ui;
+    public GameObject loadingScrn;
     public GameObject objTeleported;
     public Transform destLoc;
 
 	// Use this for initialization
 	void Start () {
         ui.SetActive(false);
+        loadingScrn.SetActive(false);
 	}
 	
-	// Update is called once per frame
 	void OnTriggerStay (Collider other) {
         ui.SetActive(true);
         if((other.gameObject.tag == "Player")&& Input.GetKeyDown(KeyCode.E)){
-            objTeleported.transform.position = destLoc.transform.position;
+            TeleportPromted();
         }
 	}
+
+    void TeleportPromted()
+    {
+        ui.SetActive(false);
+        loadingScrn.SetActive(true);
+        Invoke("PosChanger", 2);
+        loadingScrn.SetActive(false);
+    }
+
+    void PosChanger()
+    {
+        objTeleported.transform.position = destLoc.transform.position;
+    }
 
     void OnTriggerExit()
     {
